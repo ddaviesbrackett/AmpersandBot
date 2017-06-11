c3cfd3cced942cb58aefd2aa319bd627d12672f1
ddaviesbrackett <ddaviesbrackett@users.noreply.github.com>
2017-06-11 11:32:23 +0100

    more error logging for linebot

--- vendor/LINEBotTiny.php
+++ vendor/LINEBotTiny.php
@@ -92,7 +92,7 @@ class LINEBotTiny
         $response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
         if (strpos($http_response_header[0], '200') === false) {
             http_response_code(500);
-            error_log("Request failed: " . $response);
+            error_log("Request failed: header code: " . $http_response_header[0] . "response:" . $response);
         }
     }
     private function sign($body)
