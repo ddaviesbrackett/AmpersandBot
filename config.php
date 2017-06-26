cd6635dc662ba14ea15b01a9fb2b11773471b59f
ddaviesbrackett <ddaviesbrackett@users.noreply.github.com>
2017-06-26 15:41:45 +0100

    nag in the alpha, beta, gamma rooms for screenshots as well

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
-$CONF=['AMPERBOT_CHANNEL_ACCESS'=>'(put channel string here)',
- 'AMPERBOT_CHANNEL_SECRET'=>'(put secret here)'];
+$CONF=['AMPERBOT_CHANNEL_ACCESS'=>'',
+ 'AMPERBOT_CHANNEL_SECRET'=>'',
+ 'PVE_ROOM_ID' => '',
+ 'COMMANDER_ROOM_ID' => '',
+ 'ALPHA_ROOM_ID' => '',
+ 'BETA_ROOM_ID' => '',
+ 'GAMMA_ROOM_ID' => ''];
 
 define('SHEETS_CREDENTIALS_PATH', '~/.credentials/sheets.googleapis.com-php-quickstart.json');
